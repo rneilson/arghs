c5877e85f3764c2ea70b27abe295d7083734ec30
Raymond Neilson <rneilson@interloper.ca>
2016-09-13 11:34:16 -0400

    Updated demo

--- demo/parse.js
+++ demo/parse.js
@@ -2,41 +2,40 @@
 'use strict';
 
 var parsed = require('../../arghs')({
-	named: ['path', 'dest'],
-	options: {
-		'id': 'array',
-		'num': 'string',
-		'post': 'bool',
-		'verbose': 'count',
-	},
-	aliases: {
-		'i': 'id',
-		'n': 'num',
-		'p': 'post',
-		'v': 'verbose'
-	},
-	help: {
-		'id': 'use item id(s) for request',
-		'num': 'number of items to retrieve',
-		'post': 'send POST request instead of GET',
-		'verbose': 'show parsed request parameters'
-	},
-	strict: {
-		named: true,
-		unnamed: false,
-		unknown: false,
-		invalid: true
-	},
-	usage: 'Usage: $1 [OPTIONS...] <PATH> <DEST>\nParse args and display, with PATH and DEST as just...things'
+  named: ['path', 'dest'],
+  options: {
+    'default-user': 'bool',
+    'id': ['array', 'id'],
+    'num': ['string', 'n'],
+    'post': 'bool',
+    'user-id': 'string',
+    'verbose': 'count',
+  },
+  aliases: {
+    'd': 'default-user',
+    'i': 'id',
+    'n': 'num',
+    'p': 'post',
+    'u': 'user-id',
+    'v': 'verbose'
+  },
+  help: {
+    'default-user': 'use default user for request',
+    'id': 'use item id(s) for request',
+    'num': 'number of items to retrieve',
+    'post': 'send POST request instead of GET',
+    'user-id': 'use this user id for request',
+    'verbose': 'show parsed request parameters'
+  },
+  strict: {
+    unknown: false,
+    unnamed: false,
+    invalid: true
+  },
+  usage: 'Usage: $1 [OPTIONS...] <PATH> <DEST>\nParse args and display, with PATH and DEST as just...things'
 }).parse();
 
-// console.log('Original args:', process.argv.slice(2));
 console.log('Args:', parsed);
 console.log('Unnamed:', parsed._);
 console.log('Unknown:', parsed.$);
 console.log('Overflow:', parsed['--']);
-// console.log('Invalid:');
-// var invalid = Object.keys(parsed['?']);
-// for (var i = 0; i < invalid.length; i++) {
-// 	console.log('    ' + parsed['?'][invalid[i]]);
-// }
